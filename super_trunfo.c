8e9df36e110710716d3664b24eec41950703f89e
Victor Coelho <victormmcoelho@gmail.com>
2025-11-22 16:30:23 -0300

    super_trunfo.c

--- super_trunfo.c
+++ super_trunfo.c
@@ -6,13 +6,9 @@
 
 int main() {
     // Área para definição das variáveis para armazenar as propriedades das cidades
-    char estado1, estado2;
-    char codigo1[3], codigo2[3];
-    char cidade1[25], cidade2[25];
-    int  populacao1, populacao2;
-    float area1, area2;
-    float pib1, pib2;
-    int pontos1, pontos2;
+    char estado1, estado2, codigo1[3], codigo2[3], cidade1[25], cidade2[25];
+    int  populacao1, populacao2, pontos1, pontos2;
+    float area1, area2, pib1, pib2, densidade1, densidade2, capita1, capita2;
 
     // Área para entrada de dados
     printf("Insira os dados da Carta 1 conforme solicitados a seguir:\n");
@@ -46,6 +42,12 @@ int main() {
     printf("Número de Pontos Turísticos: ");
     scanf("%d", &pontos2);
 
+    //Calcular a densidade populacional e o pib per capita das cidades (Nível aventureiro)
+    densidade1 = area1 / populacao1;
+    densidade2 = area2 / populacao2;
+    capita1 = pib1 / populacao1;
+    capita2 = pib2 / populacao2;
+
     // Área para exibição dos dados da cidade
     printf("\nCarta 1:");
     printf("\nEstado: %c", estado1);
@@ -55,6 +57,8 @@ int main() {
     printf("\nÁrea: %.2f km²", area1);
     printf("\nPIB: %.2f bilhões de reais", pib1);
     printf("\nNúmero de Pontos Turísticos: %d", pontos1);
+    printf("\nDensidade Populacional: %.2f hab/km²", densidade1); //Nível aventureiro
+    printf("\nPIB per Capita: %.2f reais", capita1); //Nível aventureiro
     printf("\n\nCarta 2:");
     printf("\nEstado: %c", estado2);
     printf("\nCódigo: %s", codigo2);
@@ -63,6 +67,8 @@ int main() {
     printf("\nÁrea: %.2f km²", area2);
     printf("\nPIB: %.2f bilhões de reais", pib2);
     printf("\nNúmero de Pontos Turísticos: %d", pontos2);
+    printf("\nDensidade Populacional: %.2f hab/km²", densidade2); //Nível aventureiro
+    printf("\nPIB per Capita: %.2f reais", capita2); //Nível aventureiro
 
 return 0;
-} 
+}
